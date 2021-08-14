745deafa4ced2543def6daeea01f6d3f8eb31b1f
Arie123777 <80027154+Arie123777@users.noreply.github.com>
2021-08-14 22:32:30 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,9 +12,99 @@ local Helper = MusicEditing.Helper
 -- The following lines of code is only for demonstration
 -- Please remove them and write a proper version
 ------------------------------------------------------------
-local style = dofile("test-style.lua")
 
-local melodySong = Song.buildFromFile("country-road-melody-short.mid")
+local arguments = {input, style, output, settings}
+function readCommand(flag, argnum)
+	if (flag == "-i" or flag == "--input")
+		then
+			flag = "input"
+			if (arguments[flag] == nil)
+			then
+				arguments[flag] = arg[argnum+1]
+				local melodySong = Song.buildFromFile(arguments[flag])
+				argnumber = argnumber + 1
+				--print("i")
+			else
+				print("Repeated input flag!")
+			end
+			
+		else if (flag == "-s" or flag == "--style")
+		then
+			flag = "style"
+			if (arguments[flag] == nil)
+			then
+				arguments[flag] = arg[argnum+1]
+				local style = dofile(arguments[flag]) 
+				argnumber = argnumber + 1
+				--print("s")
+			else
+				print("Repeated style flag!")
+			end
+		else if (flag == "-o" or flag == "--output")
+		then
+			flag = "output"
+			if (arguments[flag] == nil)
+			then
+				arguments[flag] = arg[argnum+1]
+				local songExport = arguments[flag]
+				argnumber = argnumber + 1
+				--print("o")
+			else
+				print("Repeated output flag!")
+			end
+		else if (flag == "-c" or flag == "--settings")
+		then
+			flag = "settings"
+			if (arguments[flag] == nil)
+			then
+				arguments[flag] = arg[argnum+1]
+				argnumber = argnumber + 1
+				--print("c")
+			else
+				print("Repeated settings flag!")
+			end
+		else if (flag == "-h" or flag == "-v" or flag == "--version" or flag == "--help")
+		then
+			if (argnum ~= 1 or arg[2] ~= nil)
+			then
+				print("Please use this flag in single argument!")
+			else if (flag == "-h" or flag == "--help")
+				then
+					print("-h, --help\n",
+					"-v, --version\n",
+					"-i, --input MELODY_FILE_PATH\n",
+					"-s, --style STYLE_FILE_PATH\n",
+					"-o, --output OUTPUT_FILE_PATH\n",
+					"-c, --settings SETTINGS_FILE_PATH\n",
+					"example: Lua main.lua -i melody.mid -o arrangement.mid -s style.lua -c settings.lua")
+				else
+					print("Version 0, powered by LAI MAN HIN")
+				end
+			end
+		else
+			print("This flag is not available!")
+	end end end end end
+	
+end
+
+if (arg[9] ~= nil)
+	then
+	print("Too much arguments!")
+	else if (arg[1] == nil)
+		then
+		print("Please input proper argument for input and output! Use command "-h" or "-help" for more details")
+		else
+		argnumber = 1
+		while (argnumber <= 8 and arg[argnumber] ~= nil)
+		do
+			readCommand(arg[argnumber], argnumber)
+			argnumber = argnumber + 1
+		end
+	end
+end
+
+if (arguments[input] ~= nil and arguments[style] ~= nil and arguments[output] ~= nil and arguments[settings] ~= nil)
+then
 -- local melodySong = Song.buildFromFile("Debug/tempo-test.mid")
 -- local melodySong = Song.buildFromFile("c_major_scale.mid")
 -- local melodySong = Song.buildFromFile("offset-2.mid")
@@ -62,4 +152,5 @@ local arrangementContext = ArrangementContext.new(
 )
 -- melodySong:export("test.mid")
 style.arrange(arrangementContext)
-song:export("result.mid")
+song:export(arguments[output])
+end
